a9fc60eb480e815258f9e9f7bc83465efebd7c05
yugesh Eathalapaka <yugeshmessi999@gmail.com>
2021-08-03 16:00:56 +0530

    proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext } from 'apollo-link-context';
 
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://chat-s-merng.herokuapp.com/'
 });
 
 const authLink=setContext(()=>{
